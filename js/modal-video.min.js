ddeeb34d6291beb7b4c04f53e70aaf52b183215d
Hori Godai <ess_president@me.com>
2018-02-15 19:37:29 +0900

    bumping version

--- js/modal-video.min.js
+++ js/modal-video.min.js
@@ -6,7 +6,7 @@
  *   license: appleple
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 2.2.0
+ *   version: 2.2.1
  *
  * es6-object-assign:
  *   license: MIT (http://opensource.org/licenses/MIT)
